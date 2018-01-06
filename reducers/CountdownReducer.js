347536afed9cff66839d56fb79b55a6a079cdd58
Phil Czek <phil.czek@gmail.com>
2018-01-06 17:46:48 +0100

    properly removes countdowns and sorts countdowns when added

--- reducers/CountdownReducer.js
+++ reducers/CountdownReducer.js
@@ -1,12 +1,16 @@
 // new Date(new Date().getFullYear() + 1, 0, 1)
-export const countdowns = (state = [], action) => {
+export const countdowns = (state = [{date: new Date((new Date).setYear(2019)), title: 'Waschen'}], action) => {
   switch (action.type) {
     case 'ADD_COUNTDOWN':
       const {date, title} = action
-      return [...state, {date, title}]
+      return [...state, {date, title}].sort(sortByDate)
     case 'DELETE_COUNTDOWN':
-      return state.slice(1, state.countdowns.length - 1)
+      return state.slice(1, state.length)
     default:
       return state
   }
+}
+
+function sortByDate (cA, cB) {
+  return cA.date - cB.date
 }
