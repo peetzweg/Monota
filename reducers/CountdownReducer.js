6ec78606af7820e3c81120bc59b6efcbf365de02
Phil Czek <phil.czek@gmail.com>
2018-01-11 10:14:11 +0100

    adds a createdAt field inside the countdowns in order to implement other scheduling strategies in the future

--- reducers/CountdownReducer.js
+++ reducers/CountdownReducer.js
@@ -2,14 +2,20 @@ export const countdowns = (state = [], action) => {
   switch (action.type) {
     case 'ADD_COUNTDOWN':
       const {date, title} = action
-      return [...state, {date, title}].sort(sortByDate)
+      return [...state, {date, title, createdAt: new Date()}].sort(sortByDate)
     case 'DELETE_COUNTDOWN':
       return state.slice(1, state.length)
     case 'persist/REHYDRATE':
       if (action.payload) {
         const {countdowns} = action.payload
         if (countdowns) {
-          return countdowns.map(({date, ...rest}) => {return {date: new Date(date), ...rest}})
+          return countdowns.map(({date, createdAt, ...rest}) => {
+            return {
+              date: new Date(date),
+              createdAt: new date(createdAt),
+              ...rest
+            }
+          })
         }
       }
       return state
